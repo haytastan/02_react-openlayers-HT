95048f04bf009a80c45f68b1c448952cfa6db5f4
haytastan <45230816+haytastan@users.noreply.github.com>
2019-12-19 11:04:30 +0300

    Initial commit by hayati tastan

---
+++ src/custom/popup.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import * as ol from 'openlayers';
+
+import './popup.css';
+
+export class Popup extends React.Component<any, any> {
+  containerEl: HTMLElement;
+  contentEl: HTMLElement;
+  contentClose:HTMLElement;
+
+  constructor(props) {
+    console.log(2222222222);
+    super(props);
+  }
+
+  componentDidMount(){
+    this.contentClose.addEventListener("click",()=>{
+      this.containerEl.style.display='none';
+    });
+  }
+
+  render() {
+    console.log(3333333333);
+    return (
+      <div className="olPopup" ref={el => this.containerEl = el}>
+        <a className="olPopupCloser"
+          href="javascript:void(0)"
+          ref={el => this.contentClose = el}
+        ></a>
+        <div className="olPopupContents" ref={el => this.contentEl = el}></div>
+      </div>
+    );
+  }
+
+  setContents(html) {
+    this.contentEl.innerHTML = html;
+  }
+
+  show(bottomDistance: string = '12px') {
+    this.containerEl.style.bottom = bottomDistance;
+    this.containerEl.style.display = 'block';
+  }
+
+}
